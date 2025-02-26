2b6093f495a46c08af8a3f87799866babdf749ad
Than Thongsuebsaeng <6242043726@cbs.chula.ac.th>
2025-02-27 00:17:09 +0700

    feat: log user home

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import tw from 'twrnc';
 
 import { Text } from '../components/CustomText';
 import { Colors } from '../shared/style/Colors';
+import { useStore } from '../stores/useStore';
 
 const HomePage: React.FC = () => {
+  const {user} = useStore();
+  useEffect(() => {
+    console.log('user info', user);
+  }, [user]);
   return (
     <View style={tw`flex-1 items-center justify-center bg-white w-full h-full`}>
-      <Text style={tw`text-3xl my-4 text-[${Colors.primary}] border border-[${Colors.primary}] rounded-lg px-8 py-4`}>ยินดีต้อนรับ</Text>
+      <Text
+        style={tw`text-3xl my-4 text-[${Colors.primary}] border border-[${Colors.primary}] rounded-lg px-8 py-4`}>
+        ยินดีต้อนรับ
+      </Text>
     </View>
   );
 };

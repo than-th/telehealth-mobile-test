2c9927bc31ff2613996b6a37d7bd4a22551139f3
Than Thongsuebsaeng <6242043726@cbs.chula.ac.th>
2025-02-26 22:58:22 +0700

    feat: shared style

---
+++ src/shared/style/Colors.ts
@@ -0,0 +1,7 @@
+export const Colors = {
+  default: '#464646',
+  primary: '#3C6CE7',
+  secondary:'#ECF3FF',
+  empty: '#b4b4b4',
+  textField: '#CFCFCF',
+};
---
+++ src/shared/style/SharedStyle.ts
@@ -0,0 +1,6 @@
+import {StyleSheet} from 'react-native';
+export const sharedStyle = StyleSheet.create({
+  fontBold: {
+    fontFamily: 'NotoSansThai-SemiBold',
+  },
+});

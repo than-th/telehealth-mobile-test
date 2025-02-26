af44627fe01e9e64c88487e4dec81d66e23c423d
Than Thongsuebsaeng <6242043726@cbs.chula.ac.th>
2025-02-26 23:31:39 +0700

    feat: home page

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import {View} from 'react-native';
+import { View } from 'react-native';
+import tw from 'twrnc';
+
+import { Text } from '../components/CustomText';
+import { Colors } from '../shared/style/Colors';
 
 const HomePage: React.FC = () => {
   return (
-    <View />
+    <View style={tw`flex-1 items-center justify-center bg-white w-full h-full`}>
+      <Text style={tw`text-3xl my-4 text-[${Colors.primary}] border border-[${Colors.primary}] rounded-lg px-8 py-4`}>ยินดีต้อนรับ</Text>
+    </View>
   );
 };
 
